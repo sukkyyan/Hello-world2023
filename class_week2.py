98b49e9ea4f399af02e09c0ea158b019fe5b636a
Sukky147 <jessieyan98@gmail.com>
2021-09-23 11:42:47 -0400

    Create class_week2.py

---
+++ class_week2.py
@@ -0,0 +1,40 @@
+# this is an example of variables
+
+# this is a boolean
+amIalive = True
+print (amIalive)
+
+# this is a string
+myName = "Sukky Y."
+print (myName)
+
+var1 = "1"
+var2 = "4"
+var3 = "7"
+print (var1 + var2 +var3)
+
+var1 = "love"
+var2 = "Xuanze Yao"
+print (var1 + " " + var2)
+
+forHowlong = ["1year", "2years", "forever", "?"]
+x = 2
+print (forHowlong[x])
+
+myAge = 18
+print (myAge)
+
+myAge = 18
+herAge = 19
+print (myAge * herAge)
+
+# now learning objects, key value pairs
+myDictionary = {
+    "name": "sukky", 
+    "size": "big", 
+    "color": "brown-ish gold-fish", 
+    "hsaTail": True, 
+    "numberOflegs": 4
+}
+
+print(myDictionary["name"])
